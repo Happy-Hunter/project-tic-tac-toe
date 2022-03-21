832c9f2a247b8566763605d27c16d3b3ccf4d9f7
Kate Hunter <iamkatehunter@gmail.com>
2022-03-21 22:43:51 +1100

    Fuck yeah, I made a 3x3 grid in CSS

---
+++ js/tic.js
@@ -0,0 +1,12 @@
+console.log('ding-ding-ding!')
+
+let player = 'x'
+let computer = '0'
+
+let playerMoveNone = null;
+let computerMoveNone = null;
+
+const move = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const randomIndex = Math.floor(Math.random() * move.length);
+
