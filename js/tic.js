8df3dbbf14cbe1918ac3aeb8c9d0293bd1ffc985
Kate Hunter <iamkatehunter@gmail.com>
2022-03-22 15:12:31 +1100

    coming soon

--- js/tic.js
+++ js/tic.js
@@ -3,10 +3,45 @@ console.log('ding-ding-ding!')
 let player = 'x'
 let computer = '0'
 
-let playerMoveNone = null;
-let computerMoveNone = null;
+const playerMoveNone = null;
+const computerMoveNone = null;
 
 const move = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const randomIndex = Math.floor(Math.random() * move.length);
+const randomIndex = Math.random() * move.length;
+
+computer.move = move[randomIndex];
+player.move = .board-square.click()
+
+for (let i = 0; i = <= move.length; i++) {
+    if (player.move = move[0] && move[1] && move[2])
+    console.log('Player Wins!')
+    if (player.move = move[3] && move[4] && move[5])
+    console.log('Player Wins!')
+    if (player.move = move[6] && move[7] && move[8])
+    console.log('Player Wins!')
+    if (player.move = move[0] && move[3] && move[6])
+    console.log('Player Wins!')
+    if (player.move = move[1] && move[4] && move[7])
+    console.log('Player Wins!')
+    if (player.move = move[2] && move[5] && move[8])
+    console.log('Player Wins!')
+    if (player.move = move[0] && move[4]&& move [8])
+    console.log('Player Wins!')
+    if (player.move = move[2] && move[4] && move[6])
+    console.log('Player Wins!')
+    //if computer move =is player move don't allow
+
+    
+}
+    
+
+    
+
+
+
+
+
+
+//console.log(computer)
 
